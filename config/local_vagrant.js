665219cfeed062dcfae93563dd23b2b77557780e
Hekku2 <hekku2@gmail.com>
2016-01-06 19:34:01 +0200

    Added vagrant config for backend

---
+++ config/local_vagrant.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * Local environment settings for vagrant dev environment
+ */
+module.exports = {
+  connections: {
+    mysql: {
+      adapter: 'sails-mysql',
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'home-dashboard',
+      charset: 'utf8',
+      collation: 'utf8_swedish_ci'
+    }
+  },
+  models: {
+    connection: 'mysql'
+  },
+  session: {
+    secret: 'VAGRANT-SECRET'
+  },
+  port: 1337,
+  environment: 'development',
+  log: {
+    level: 'info'
+  }
+};
